e2259ad913572d440c9c54ceb3b59c21194f90a1
Carlos Vasquez <sergio.camachoo@misena.edu.co>
2021-06-03 22:18:16 -0500

    Avances 3

--- src/config/global.js
+++ src/config/global.js
@@ -42,22 +42,22 @@ export default {
       },
     ],
     subMenu: [
-      {
-        nombreRuta: 'actividad',
-        icono: 'far fa-question-circle',
-        titulo: 'Actividad didáctica',
-        desarrolloContenidos: true,
-      },
+      // {
+      //   nombreRuta: 'actividad',
+      //   icono: 'far fa-question-circle',
+      //   titulo: 'Actividad didáctica',
+      //   desarrolloContenidos: true,
+      // },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
       },
-      {
-        nombreRuta: 'complementario',
-        icono: 'far fa-folder-open',
-        titulo: 'Material complementario',
-      },
+      // {
+      //   nombreRuta: 'complementario',
+      //   icono: 'far fa-folder-open',
+      //   titulo: 'Material complementario',
+      // },
       {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
@@ -171,135 +171,95 @@ export default {
   },
   referencias: [
     {
+      referencia: 'Asale. (2021). Moda. Diccionario de la lengua española.',
+      link: 'https://dle.rae.es/moda',
+    },
+    {
       referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA: ',
+        'Asale. (2021). Sastre, sastra. Diccionario de la lengua española.',
+      link: 'https://dle.rae.es/sastre',
+    },
+    {
+      referencia: 'Butterick 7660. (2021). Elegancia en la moda. Flickr.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://www.flickr.com/photos/graustark/3372648147/in/set-72157611750347036',
     },
     {
       referencia:
-        'Castellanos, O., Fumeque A., y Ramirez D. (2011). Análisis de tendencias: de la información hacia la innovación. Universidad Nacional de Colombia. Sistema de Bibliotecas SENA: ',
+        'Cobo, M., E., Acuña, R., O. (2019). Belleza, moda y elegancia en Colombia vista a través de la revista cromos, 1916-1929. Tzintzun. Revista de Estudios Históricos, (70), 87-120.',
+      link: 'https://www.redalyc.org/jatsRepo/898/89862270006/html/index.html',
+    },
+    {
+      referencia:
+        'Inexmoda. (2021). Inexmoda y Fashion Snoops, empoderando a las marcas a aprovechar las tendencias del futuro',
+      link: 'https://www-fashionsnoops-com.bdigital.sena.edu.co/Default.aspx',
+    },
+    {
+      referencia:
+        'Mesa, F., A. L. (2014).  Historia del oficio de la modista en Medellín, 1940 – 1980 procesos de individuación y prácticas de subjetivación. [Tesis de maestría, Universidad Nacional de Colombia, sede Medellín]. Repositorio institucional UNAL.',
+      link: 'https://repositorio.unal.edu.co/handle/unal/21097',
+    },
+    {
+      referencia: 'Moda sin estereotipos. (2017). Pirámide de Maslow moda.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000007947',
+        'http://modasinestereotipos.es/5-niveles-de-la-piramide-de-maslow-para-una-moda-sin-estereotipos/',
     },
     {
       referencia:
-        'Harpersbazaar. (2019). La ropa que eliges afecta a tu estado de ánimo y cerebro, según expertos. En arpersbazaar.mx.',
-    },
-    {
-      referencia:
-        'Lafuente M., Navarro J., y Navarro J. (2005). Ilustración de moda. Sistema de Bibliotecas SENA',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000025549',
-    },
-    {
-      referencia:
-        'Laver, J.,  Albizua, H., y Fortea, B. (1990). Breve historia del traje y la moda. Sistema de Bibliotecas SENA. ',
-    },
-    {
-      referencia:
-        'Navarro, J.,  y Lafuente, M. (2010). Ilustración de moda: dibujo plano. Sistema de Bibliotecas SENA:',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/q6j6k0/sena_aleph000023241',
-    },
-    {
-      referencia:
-        'Áciedad Americana de Ensayos y Materiales (ASTM). (2003). Textiles. Editorial Staff. [Book]. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000051551',
-    },
-    {
-      referencia:
-        'Szkutnicka, B., Cuenca, C., Koyama, A. (2010). El dibujo técnico de moda paso a paso. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+        'Semana. (23 de julio de 2017). Estas fueron las primeras puntadas de la moda en Colombia.',
+      link: 'https://www.semana.com/historia-de-la-moda-en-colombia/533547/',
     },
   ],
   glosario: [
     {
-      termino: 'MoodBoard',
+      termino: 'Bidimensional',
       significado:
-        'su traducción literal es mapa de inspiración y aunque su uso está muy asociado al ámbito de la decoración de interiores o la moda, <br/>realmente se utiliza en entornos creativos muy diversos, desde el diseño de productos o servicios al audiovisual o el diseño gráfico.',
-    },
-    {
-      termino: 'Suftware',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Pirognóstico',
-      significado:
-        'ensayo sin componentes reactivos, quiere decir, reconocimiento de los componentes, mediante la aplicación adecuada del fuego.',
-    },
-    {
-      termino: 'Áectores',
-      significado:
-        'en software de diseño, son líneas que se crean y permiten, expandir, modificar, al tamaño que sea sin pixelarse porque se unen por coordenadas matemáticas del programa y no se generan por números fijos.',
-    },
-    {
-      termino: 'Moda',
-      significado:
-        'hábito repetitivo que identifica a una persona, una especie de presión social, que debe consumir, utilizar o hacer.',
+        'Se representa según su altura y su anchura, y no de acuerdo a su profundidad.',
     },
     {
       termino: 'Diseño',
       significado:
-        'actividad creativa orientada a soluciones idóneas a ciertas problemáticas.',
+        'Actividad creativa orientada a las soluciones idóneas de ciertas problemáticas.',
+    },
+    {
+      termino: 'Moda',
+      significado:
+        'Hábito repetitivo que identifica a una persona, una especie de presión social, sobre qué debe consumir, utilizar o hacer.',
     },
     {
       termino: 'Tipología',
       significado:
-        'estudio de los tipos o modelos que se usan para clasificar en diversas ciencias o disciplinas.',
-    },
-    {
-      termino: 'Bidimensional',
-      significado:
-        'que se representa según su altura y su anchura y no su profundidad.',
-    },
-    {
-      termino: 'Interés Inferior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interseccionalidad',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interés superior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+        'Estudio de los tipos o modelos que se usan para clasificar en diversas ciencias o disciplinas.',
     },
   ],
-  complementario: [
-    {
-      texto:
-        'BIC., (2015). “2 minutos para entender el desarrollo sostenible – Spanish”',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
-    },
-    {
-      texto:
-        'Arnold, M. y Osorio, F., (1998). Introducción a los conceptos básicos de la Teoría general de sistemas. Cinta moebio 3: 40-49',
-      tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
-    },
-    {
-      texto:
-        'Balboa, C. y Dominguez, M. (2014). Economía circular como marco para el ecodiseño: el modelo ECO-3. Informador Técnico (Colombia) Volumen 78, No. 1. Pág. 52-90.',
-      tipo: 'Artículo científico',
-      link: 'https://www.google.com/',
-    },
-    {
-      texto: 'Economía de Rosquilla. (s.f.).',
-      tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
-    },
-    {
-      texto: '2 minutos para entender el desarrollo sostenible – Spanish',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
-    },
-  ],
+  // complementario: [
+  //   {
+  //     texto:
+  //       'BIC., (2015). “2 minutos para entender el desarrollo sostenible – Spanish”',
+  //     tipo: 'Video',
+  //     link: 'https://www.google.com/',
+  //   },
+  //   {
+  //     texto:
+  //       'Arnold, M. y Osorio, F., (1998). Introducción a los conceptos básicos de la Teoría general de sistemas. Cinta moebio 3: 40-49',
+  //     tipo: 'Página web',
+  //     descarga: '/downloads/prueba.pdf',
+  //   },
+  //   {
+  //     texto:
+  //       'Balboa, C. y Dominguez, M. (2014). Economía circular como marco para el ecodiseño: el modelo ECO-3. Informador Técnico (Colombia) Volumen 78, No. 1. Pág. 52-90.',
+  //     tipo: 'Artículo científico',
+  //     link: 'https://www.google.com/',
+  //   },
+  //   {
+  //     texto: 'Economía de Rosquilla. (s.f.).',
+  //     tipo: 'Página web',
+  //     descarga: '/downloads/prueba.pdf',
+  //   },
+  //   {
+  //     texto: '2 minutos para entender el desarrollo sostenible – Spanish',
+  //     tipo: 'Video',
+  //     link: 'https://www.google.com/',
+  //   },
+  // ],
 }
