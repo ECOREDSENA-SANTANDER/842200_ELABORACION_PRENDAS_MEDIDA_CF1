5b26b7bbe6e7a18ca2324d76435c9b2b41608ecf
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-08-29 19:55:48 -0500

    Cargue de Actividad Didáctica

--- src/config/global.js
+++ src/config/global.js
@@ -56,12 +56,12 @@ export default {
       },
     ],
     subMenu: [
-      // {
-      //   nombreRuta: 'actividad',
-      //   icono: 'far fa-question-circle',
-      //   titulo: 'Actividad didáctica',
-      //   desarrolloContenidos: true,
-      // },
+      {
+        nombreRuta: 'actividad',
+        icono: 'far fa-question-circle',
+        titulo: 'Actividad didáctica',
+        desarrolloContenidos: true,
+      },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
